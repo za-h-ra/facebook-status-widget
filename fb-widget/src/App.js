d105a369755073f81d24eedaab8a08b7da661174
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-17 18:18:50 -0500

    import components into app.js

--- fb-widget/src/App.js
+++ fb-widget/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Status from './Components/Status'
+import Comment from './Components/Comment'
+import Like from './Components/Like'
+import LikeIcon from './Components/LikeIcon'
 
 export default function App() {
     return (
         <>
             <h1>Hello World!</h1>
+            <Status />
+            <Comment />
+            <Like />
+            <LikeIcon />
         </>
     )
 }
